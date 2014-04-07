819aecc76f99e97df95c060fc13f361516fe7e02
Marco Buss <marco.buss@opitz-consulting.com>
2014-04-07 14:52:44 +0200

    chore: jslint test

--- Gruntfile.js
+++ Gruntfile.js
@@ -269,7 +269,7 @@ module.exports = function(grunt) {
     install();
   });
   grunt.registerTask('curl', 'curl-dir'); //alias
-  grunt.registerTask('ci', ['build','jshint:ci','karma:ci','ngdocs']);
+  grunt.registerTask('ci', ['build','karma:ci','ngdocs']);
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
